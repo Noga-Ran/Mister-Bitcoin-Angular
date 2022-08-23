c393a7af19a684bfd99d0022aa3ffa02e46066a5
Noga <nogaran01@gmail.com>
2022-08-23 14:42:31 +0300

    about

---
+++ src/app/cmps/moves-list/moves-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'moves-list',
+  templateUrl: './moves-list.component.html',
+  styleUrls: ['./moves-list.component.scss']
+})
+export class MovesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
